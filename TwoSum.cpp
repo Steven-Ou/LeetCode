127038a9527c69c8645e986938f4d2e0f0b813fe
Steven-Ou <osteve425@gmail.com>
2025-01-10 14:48:48 +0800

    created a forloop

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -4,7 +4,10 @@ using namespace std;
 class Solution{
    public: 
         vector<int> TwoSum(vector<int>& nums, int target){
-            
+            int n = nums.size();
+            for(int i=0;i<n-1;i++){
+                
+            }
             return {};
         }
 };
