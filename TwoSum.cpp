745bcad2fa8a7f3ec4afea4060999455bbeec696
Steven-Ou <osteve425@gmail.com>
2025-01-10 14:51:11 +0800

    completed the if statement inside the loop

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -6,7 +6,11 @@ class Solution{
         vector<int> TwoSum(vector<int>& nums, int target){
             int n = nums.size();
             for(int i=0;i<n-1;i++){
-                
+                for(int j= i+1; j<n; j++){
+                    if(nums[i]+ nums[j]== target){
+                        return{i,j};
+                    }
+                }
             }
             return {};
         }
