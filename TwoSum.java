42754690f641e342febfc7cfac1bb94a6ea16bb2
Steven-Ou <osteve425@gmail.com>
2025-01-11 12:10:39 +0800

    commenting

--- TwoSum.java
+++ TwoSum.java
@@ -10,3 +10,25 @@ class Solution{
             return new int[] {};  
     }
 }
+/*
+ * A even faster solution
+ * class Solution {
+    public int[] twoSum(int[] nums, int target) {
+  
+
+
+        for (int i=1; i<nums.length; i++)
+        
+            for(int j=i;j<nums.length; j++)
+            
+                if((nums[j]+nums[j-i]) == target)
+                
+                    return new int[]{j,j-i};
+                
+            
+        
+        
+        return null; 
+    }
+}
+ */
