53d3ff5f235dd8712ec7cb643b201b7a0517f1ca
Steven-Ou <osteve425@gmail.com>
2025-01-11 12:12:00 +0800

    fixing the structure

--- TwoSum.java
+++ TwoSum.java
@@ -14,20 +14,10 @@ class Solution{
  * A even faster solution
  * class Solution {
     public int[] twoSum(int[] nums, int target) {
-  
-
-
         for (int i=1; i<nums.length; i++)
-        
             for(int j=i;j<nums.length; j++)
-            
                 if((nums[j]+nums[j-i]) == target)
-                
                     return new int[]{j,j-i};
-                
-            
-        
-        
         return null; 
     }
 }
