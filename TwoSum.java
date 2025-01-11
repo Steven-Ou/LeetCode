5252bd555186e68c28fa20a062c404ad5573684b
Steven-Ou <osteve425@gmail.com>
2025-01-11 11:46:01 +0800

    created an if statement

--- TwoSum.java
+++ TwoSum.java
@@ -3,7 +3,7 @@ class Solution{
         int x = nums.length;
         for(int i=0; i<x-1;i++){
             for(int j=i+1;j<x;j++){
-            
+                if(nums[i]+nums[j]==target)
             }
         }
                 
