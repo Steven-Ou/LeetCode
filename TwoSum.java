4b98032053a8f06a65e65144ca8bc92263d4c108
Steven-Ou <osteve425@gmail.com>
2025-01-11 11:55:21 +0800

    fixing mistakes

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,6 @@
 class Solution{
     public int[] TwoSum(int[] nums, int target){
-        for(int i=0; i<x-1;i++){
+        for(int i=0; i<nums.length-1;i++){
             for(int j=i+1;j<x;j++){
                 if(nums[i]+nums[j]==target){
                     return new int[] {i,j};
