15e61291b9659b308ced7031fcf441afc8acdecf
Steven-Ou <osteve425@gmail.com>
2025-01-11 11:45:32 +0800

    another loop

--- TwoSum.java
+++ TwoSum.java
@@ -2,7 +2,9 @@ class Solution{
     public int[] TwoSum(int[] nums, int target){
         int x = nums.length;
         for(int i=0; i<x-1;i++){
+            for(int j=i+1;j<x;j++){
             
+            }
         }
                 
     }
