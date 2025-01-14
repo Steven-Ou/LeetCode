b3b9a18ae7f053859a9db5fc9816de61496bb200
Steven-Ou <osteve425@gmail.com>
2025-01-14 10:51:34 +0800

    comments

--- TwoNum.cpp
+++ TwoNum.cpp
@@ -5,7 +5,7 @@ class solution{
         int carry =0;
 
         while(l1 != nullptr|| l2!= nullptr || carry!=0){
-            int d1 = (l1 != nullptr) ? l1->val:0;
+            int d1 = (l1 != nullptr) ? l1->val:0; //If l1 has number in it, it will be set to d1, else it will be set to 0. 
             int d2 = (l2!=nullptr) ? l2->val:0;
         }
     }
