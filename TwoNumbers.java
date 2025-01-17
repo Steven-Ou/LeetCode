f64734baa5e2a4f026849df108df6c503aa04c11
Steven-Ou <osteve425@gmail.com>
2025-01-17 11:28:07 +0800

    ListNode

--- TwoNumbers.java
+++ TwoNumbers.java
@@ -2,6 +2,6 @@ import java.lang.classfile.components.ClassPrinter.ListNode;
 
 class Solution{
     public ListNode addTwoNumbers(ListNode l1, ListNode l2){
-        
+        ListNode dum = new ListNode(0);
     }
 }
