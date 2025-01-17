33d48e6d7a22c9a7b1220faf0f0db6590fd3ec58
Steven-Ou <osteve425@gmail.com>
2025-01-17 12:36:56 +0800

    new var

--- TwoNumbers.java
+++ TwoNumbers.java
@@ -3,6 +3,8 @@ import java.lang.classfile.components.ClassPrinter.ListNode;
 class Solution{
     public ListNode addTwoNumbers(ListNode l1, ListNode l2){
         ListNode dum = new ListNode(0);
-        
+        ListNode n = dum; 
+
+
     }
 }
