a8e5c474148cff7876e296b987301cef3fd727a9
Steven-Ou <osteve425@gmail.com>
2025-01-19 22:37:38 +0800

    comments

--- L1.cpp
+++ L1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution{
    public: 
         vector<int> TwoSum(vector<int>& nums, int target){
-            int n = nums.size();
+            int n = nums.size(); //initializing a variable and storing the size of nums
             for(int i=0;i<n-1;i++){
                 for(int j= i+1; j<n; j++){
                     if(nums[i]+ nums[j]== target){
