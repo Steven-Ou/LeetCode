013b1a7e7ef1a82ddc67d4556e95cf07761b6405
Steven-Ou <osteve425@gmail.com>
2025-01-20 13:33:30 +0800

    comments

--- L1.cpp
+++ L1.cpp
@@ -8,7 +8,7 @@ class Solution{
             for(int i=0;i<n-1;i++){
                 for(int j= i+1; j<n; j++){
                     if(nums[i]+ nums[j]== target){
-                        return{i,j};
+                        return{i,j}; // returning the two nums
                     }
                 }
             }
