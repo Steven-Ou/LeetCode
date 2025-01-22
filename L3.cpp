bc7ef41f1f95eeebdb413a3c0badd4ef179ac456
Steven-Ou <osteve425@gmail.com>
2025-01-22 16:10:52 +0800

    created a vector

--- L3.cpp
+++ L3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
    public:
     int lengthOfLongestSubstring(string S){
+        vector <int> store; 
         for(int i =0; i< S.length();i++){
             
         }
