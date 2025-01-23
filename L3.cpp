75852c1892a49956199bdeb2bc2d0021488a4619
Steven-Ou <osteve425@gmail.com>
2025-01-23 09:42:33 -0500

    created another loop

--- L3.cpp
+++ L3.cpp
@@ -10,6 +10,9 @@ class Solution {
         for(int i =0; i< S.length();i++){
             store[i] == S[i];
             temp[i] == store[i];
+            for(int j =0; j<S.length();i++){
+                
+            }
         }
         
     }
