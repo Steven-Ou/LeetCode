2aa90b73d40e66e9e7e7ad6366f4226fd5956cc0
Steven-Ou <osteve425@gmail.com>
2025-01-23 10:07:32 -0500

    fixing the name

--- L3.cpp
+++ L3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution {
    public:
     int lengthOfLongestSubstring(string S){
-        unorder_set<char> substring; //Tracking characters in the current substring
+        unordered_set<char> substring; //Tracking characters in the current substring
         int Mleng =0; //Max length
         int start =0; //goes through every letter in the substring
         for(int i =0; i< S.length();i++){
