fc8e11b50e1811327a057f9c35e76ec3f64dba7e
Steven-Ou <osteve425@gmail.com>
2025-01-23 21:07:54 -0500

    letting the remain getting the %10 of num

--- L2119.cpp
+++ L2119.cpp
@@ -6,7 +6,8 @@ class Solution{
         bool isSameAfterReversals(int num){
             int r =0; 
             while(num!=0){
-                
+                int remain = num%10;
+                r = r *10 + remain;
             }
         }
 };
