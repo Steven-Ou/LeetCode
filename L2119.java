47d086977b84bd2891d9f2213ffa19ea64715f30
Steven-Ou <osteve425@gmail.com>
2025-01-25 09:55:08 -0500

    first condition of the while loop

--- L2119.java
+++ L2119.java
@@ -2,6 +2,9 @@ class Solution{
     public boolean isSameAfterReversals(int num){
         int s = num; 
         int r = 0;  //reversal 1
-        while
+        while(num!=0){
+            r = num%10 + r*10; 
+            
+        }
     }
 }
