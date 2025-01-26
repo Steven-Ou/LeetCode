edcefcaaba6266527acf098191e9a3b9d5f9aae5
Steven-Ou <osteve425@gmail.com>
2025-01-26 11:08:30 -0500

    int n and maxl

--- L3.java
+++ L3.java
@@ -2,8 +2,9 @@ import java.util.HashSet;
 
 class Solution{
     public int lengthOfLongestSubstring(String s){
-        Set<Character> substring = new HashSet<>();
+        int n = s.length(); 
         int MaxL = 0; 
+        Set<Character> substring = new HashSet<>();
         int first =0; 
     
     }   
