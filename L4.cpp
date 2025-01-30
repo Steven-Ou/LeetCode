c1e71ebee95123a4324f2b112871ef09833b4eba
Steven-Ou <osteve425@gmail.com>
2025-01-29 20:18:00 -0500

    comments

--- L4.cpp
+++ L4.cpp
@@ -5,6 +5,7 @@ class Solution {
     public: 
     double findMedianSortedArrays(vector<int> num1, vector<int> num2){
         vector<int>v;
+        //storing each nums into an array
         for(auto num:num1){
             v.push_back(num);
         }
