2aeeaef7367d20ae0bbfb83eafadbe818fabc225
Steven-Ou <osteve425@gmail.com>
2025-01-29 20:16:46 -0500

    for loop

--- L4.cpp
+++ L4.cpp
@@ -5,6 +5,8 @@ class Solution {
     public: 
     double findMedianSortedArrays(vector<int> num1, vector<int> num2){
         vector<int>v;
-
+        for(auto num:num1){
+            v.push_back(num);
+        }
     }
 };
