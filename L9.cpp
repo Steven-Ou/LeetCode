c3c3ab4369e06ad4bfb4191b05ae13db771e7d56
Steven-Ou <osteve425@gmail.com>
2025-02-01 11:18:41 -0500

    fixing variable

--- L9.cpp
+++ L9.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution{
     public:
         bool isPalindrome(int x){
-            vector <int> store;
+            int store;
             int s; 
             while(x!=0){
                 store = x%10;
