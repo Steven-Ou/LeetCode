d511ea27725f7b108de6b41ceddcfe00e21f9b45
Steven-Ou <osteve425@gmail.com>
2025-02-02 11:22:07 -0500

    replaced

--- L9.java
+++ L9.java
@@ -3,6 +3,8 @@ class Solution{
        if(x<0){
         return false; 
        }
+       int a  = x;  //stores x
+       long rev = 0; 
        while(x!=0){
             int rem = x%10;
             rev = rev * 10 + rem; 
