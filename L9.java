d8c4239e516c2ad74b10018fd427c3bf90456a32
Steven-Ou <osteve425@gmail.com>
2025-02-02 11:17:28 -0500

    if condition to return false

--- L9.java
+++ L9.java
@@ -2,7 +2,9 @@ class Solution{
     public  boolean isPalindrome(int x){
        int a  = x;  //stores x
        long rev = 0; 
-       
+       if(x<0){
+        return false; 
+       }
     }
     
 }
