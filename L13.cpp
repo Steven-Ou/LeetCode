694042145a91288a86cbb486f11fc3d6dacef0e4
Steven-Ou <osteve425@gmail.com>
2025-02-04 00:23:28 -0500

    fixing

--- L13.cpp
+++ L13.cpp
@@ -13,7 +13,7 @@ class Solution{
             int D = 500;
             int M =1000;
             vector <string> store; 
-            for(char i = 0; i< s.length(); i++){
+            for(int i = 0; i< s.length(); i++){
                 if(s[i]== "I" || s[i] == "V" || s[i] == "X"|| s[i] == "L"|| s[i] == "C" ||s[i]=="D"|| s[i]=="M"){
                     store += s; 
                 }
