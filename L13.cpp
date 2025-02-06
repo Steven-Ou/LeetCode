1a2a5532bf6e2f903c166febf4ada33a980121c4
Steven-Ou <osteve425@gmail.com>
2025-02-05 23:28:59 -0500

    even more faster MS solution

--- L13.cpp
+++ L13.cpp
@@ -54,5 +54,37 @@ class Solution{
         }
 };
 /*
-
+class Solution {
+public:
+    int valuess(char t){
+        switch(t){
+           case 'I': return 1;
+           case 'V': return 5;
+           case 'X': return 10;
+           case 'L': return 50;
+           case 'C': return 100;
+           case 'D': return 500;
+           case 'M': return 1000;
+            
+        }
+        return -1;
+    }
+    int romanToInt(string s) {
+        int c,n=0;
+        char st;
+        for(int i=0;i<s.length();i++){
+           c=valuess(s[i]);
+           if(c==-1){
+            return -1;
+           }
+           if(valuess(s[i+1])>valuess(s[i])){
+            n-=c;
+           }
+           else{
+            n+=c;
+           }
+        }
+        return n;
+    }
+};
 */
