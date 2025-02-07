c35694e20eaf433bf3976aa70abed8c882ab6127
Steven-Ou <osteve425@gmail.com>
2025-02-07 13:38:51 -0500

    second case

--- L13.java
+++ L13.java
@@ -2,7 +2,7 @@ class Solution{
     int val(char t){
         switch (t) {
             case 'I': return 1;
-            
+            case 'V': return 5; 
         } 
     }
     public int romanToInt(String s){
