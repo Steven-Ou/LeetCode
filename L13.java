95bdfe5dc56e6ee44bc22bf735ff12b796d8fc88
Steven-Ou <osteve425@gmail.com>
2025-02-07 21:10:48 -0500

    case 4

--- L13.java
+++ L13.java
@@ -4,7 +4,7 @@ class Solution{
             case 'I': return 1;
             case 'V': return 5; 
             case 'X': return 10;
-            
+            case 'L': return 50;
         } 
     }
     public int romanToInt(String s){
