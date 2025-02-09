c6459847d9088a629d180c9194b71faee2297917
Steven-Ou <osteve425@gmail.com>
2025-02-09 12:03:59 -0500

    if statement

--- L13.java
+++ L13.java
@@ -16,7 +16,9 @@ class Solution{
         char st;
         for(int i=0;i<s.length();i++){
             c = val(s[i]);
-            
+            if(c==-1){
+                return -1;
+            }
         }
 
     }    
