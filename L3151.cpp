549b20633383128f5e9a932df622e4b1b148c729
Steven-Ou <osteve425@gmail.com>
2025-02-13 00:28:07 -0500

    set s =nums

--- L3151.cpp
+++ L3151.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution{
     public:
         bool isArraySpecial(vector<int>& nums){
-            vector <int> s; 
+            vector <int> s = nums; 
             for(int i =0; i<nums; i++){
                 
             }
