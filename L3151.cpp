a456db2e16b63fca6f9d112a477c4c1e37bc77cf
Steven-Ou <osteve425@gmail.com>
2025-02-13 00:27:02 -0500

    created a vector

--- L3151.cpp
+++ L3151.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution{
     public:
         bool isArraySpecial(vector<int>& nums){
-            
+            vector <int> s; 
             for(int i =0; i<nums)
         }
 };
