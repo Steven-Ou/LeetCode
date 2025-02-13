b1f6bdb19ba4a1f21432e551ddb0d88eaad2365c
Steven-Ou <osteve425@gmail.com>
2025-02-13 00:39:45 -0500

    fixing

--- L3151.cpp
+++ L3151.cpp
@@ -6,7 +6,7 @@ class Solution{
             vector <int> s = nums; 
             for(int i =0; i<nums; i++){
                 int temp; 
-                temp == nums[i];
+                temp = nums[i];
                 if((temp %2== nums[i+1]%2)){
                     return false;
                 }
