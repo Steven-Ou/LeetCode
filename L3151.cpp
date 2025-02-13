8160270830b0eef5ec8b919c5816f3f4d992d6f6
Steven-Ou <osteve425@gmail.com>
2025-02-13 00:27:12 -0500

    for loop

--- L3151.cpp
+++ L3151.cpp
@@ -4,7 +4,9 @@ class Solution{
     public:
         bool isArraySpecial(vector<int>& nums){
             vector <int> s; 
-            for(int i =0; i<nums)
+            for(int i =0; i<nums; i++){
+                
+            }
         }
 };
 int main(){
