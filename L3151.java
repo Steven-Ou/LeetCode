b658a486cbae71f4642a392e98a87600bfb8f951
Steven-Ou <osteve425@gmail.com>
2025-02-16 00:46:07 -0500

    fixing the size

--- L3151.java
+++ L3151.java
@@ -1,9 +1,9 @@
 class Solution{
     public boolean isArraySpecial(int[] nums){
-        if(nums.size()<=1){
+        if(nums.length<=1){
             return true;
         }
-        for(int i=0; i<nums.length()-1;i++){
+        for(int i=0; i<nums.length-1;i++){
             int temp;
         }
     }
