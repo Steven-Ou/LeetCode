ca0319ade50e4962af06f037bc49502086587910
Steven-Ou <osteve425@gmail.com>
2025-02-21 00:13:31 -0500

    changing data type

--- L14.cpp
+++ L14.cpp
@@ -4,9 +4,9 @@ using namespace std;
 class Solution{
     public:
         string longestCommonPrefix(vector<string>& strs){
-            string store = strs; 
+            vector<string> store = strs; 
             for(char i =0; i<strs.size(); i++){
-                
+                if(store.substr())    
             }
         }
   
