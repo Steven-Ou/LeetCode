069d659f62df61ef4cf583944d75a2d33fee6dc7
Steven-Ou <osteve425@gmail.com>
2025-02-21 21:00:48 -0500

    \n

--- L14.cpp
+++ L14.cpp
@@ -5,6 +5,7 @@ class Solution{
     public:
         string longestCommonPrefix(vector<string>& strs){
             vector<string> store = strs; 
+            
             for(char i =0; i<strs.size(); i++){
                 if(store.substr([i])+store.substr([i+1]))    
             }
