da3904406edcfb89a063a7bd80e4d8684836fc4f
Steven-Ou <osteve425@gmail.com>
2025-02-22 15:50:40 -0500

    \n \n

--- L14.cpp
+++ L14.cpp
@@ -4,8 +4,10 @@ using namespace std;
 class Solution{
     public:
         string longestCommonPrefix(vector<string>& strs){
-            vector<string> store = strs; 
             
+            
+            vector<string> store = strs; 
+
             for(char i =0; i<strs.size(); i++){
                 if(store.substr([i])+store.substr([i+1]))    
             }
