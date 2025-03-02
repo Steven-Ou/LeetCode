bcd57dab07b17aa5a1d2408a59ffe65fe8bcfecb
Steven-Ou <osteve425@gmail.com>
2025-03-01 21:17:05 -0500

    fixing

--- L14.cpp
+++ L14.cpp
@@ -8,7 +8,7 @@ class Solution{
                 return "";
             }
             
-            string store = strs; 
+            string store = strs[0]; 
 
             for(char i =0; i<strs.size(); i++){
                 if(store.substr([i])+store.substr([i+1]))    
