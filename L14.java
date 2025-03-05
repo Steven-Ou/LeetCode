f21da86498e27da4e188dc5ede7ff8f2de04f87d
Steven-Ou <osteve425@gmail.com>
2025-03-05 15:34:22 -0500

    fixing

--- L14.java
+++ L14.java
@@ -1,13 +1,4 @@
 class Solution{
     public String longestCommonPrefix(String[] strs){
-        if(strs.isEmpty()){
-            return "";
-        }
-    }
-    public boolean isEmpty(String[] arr){
-        if(arr.length== 0){
-            return true;
-        }
-        return false;
     }
 }
