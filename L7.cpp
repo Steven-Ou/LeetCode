93d18a737a330a9fa9a1bb23f238f98060d6b4e0
Steven-Ou <osteve425@gmail.com>
2025-03-15 12:13:15 -0400

    \n

--- L7.cpp
+++ L7.cpp
@@ -4,9 +4,10 @@ using namespace std;
 class Solution{
     public:
         int reverse(int x){
-            int num=0;
+            long long num=0;
             while(x!=0){
                 int store = x %10; 
+                
                 num = num *10 + store; 
                 x/=10; 
             }
