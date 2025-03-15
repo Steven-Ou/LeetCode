fdfbb9375423f578493d4f297bd54ef7aa032abf
Steven-Ou <osteve425@gmail.com>
2025-03-15 12:14:18 -0400

    ending exception

--- L7.cpp
+++ L7.cpp
@@ -7,7 +7,9 @@ class Solution{
             long long num=0;
             while(x!=0){
                 int store = x %10; 
-                
+                if((num> INT_MAX/10)||(num<INT_MIN/10)){
+                    return 0;
+                }
                 num = num *10 + store; 
                 x/=10; 
             }
