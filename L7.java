d51f09d9992c6d15bbaeae1c1d75f9b823d7892a
Steven-Ou <osteve425@gmail.com>
2025-03-24 18:11:43 -0400

    not working needs fixing

--- L7.java
+++ L7.java
@@ -1,6 +1,7 @@
 class Solution{
     public int reverse(int x){
         int num =Math.abs(x);
+        
         while(x!=0){
             int store = x%10;
             if(num >(Integer.MAX_VALUE - store)) {
