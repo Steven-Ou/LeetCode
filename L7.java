ee7c1861ac4e7a0580588c566b9ae0de86d1875b
Steven-Ou <osteve425@gmail.com>
2025-03-27 00:32:51 -0400

    changing the var name

--- L7.java
+++ L7.java
@@ -1,8 +1,8 @@
 class Solution{
     public int reverse(int x){
         int reverse =0; 
-        while(num!=0){
-            int store = num%10;
+        while(x!=0){
+            int store = x%10;
             if(reverse >(Integer.MAX_VALUE - store)/10) {
                 return 0;
             }
