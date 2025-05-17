604d693da5337fda5b20b5dd2b8ae11d326e07ea
Steven-Ou <osteve425@gmail.com>
2025-05-17 12:03:23 -0400

    commenting

--- L1.java
+++ L1.java
@@ -1,7 +1,9 @@
 //Problem 1 of leet code
 class Solution{
     public int[] TwoSum(int[] nums, int target){
-        for(int i=0; i<nums.length-1;i++){
+        for(int i=0; i<nums.length-1;i++){ //Two loops
+            //The first loop will iterate through the array
+            //The second loop will iterate through the array starting from the next index of the first loop
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
                     return new int[] {i,j};
