c7ad92e4548c07a29a257d3e53c2515210c253c9
Steven-Ou <osteve425@gmail.com>
2025-05-19 22:11:27 -0400

    comments

--- ListNode.java
+++ ListNode.java
@@ -1,7 +1,7 @@
 
 public class ListNode {
     int val; //initialize the value of the node
-    ListNode next;
+    ListNode next; //
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
