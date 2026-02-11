bfef27addd9687a95f777c4166e593b1c517d8e2
Steven-Ou <osteve425@gmail.com>
2026-02-11 00:26:38 -0500

    Reogranizing the whole directory

---
+++ problem.cpp
@@ -0,0 +1,4 @@
+/*Create a password that doesnt accept the word hey,bye,password, curse words anyone of these words!
+password has to be 9 words long less than 32 letters
+password must contain 2 punctuation, 2 letter or more, 2 capital letters, must have 2 more math symbols,  */
+
