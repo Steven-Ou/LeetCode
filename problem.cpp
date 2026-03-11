10cd5d3cb28b72098b3f74e3267dbddbca0464a9
Steven-Ou <osteve425@gmail.com>
2026-03-11 00:35:53 -0400

    Comments

--- problem.cpp
+++ problem.cpp
@@ -2,3 +2,9 @@
 password has to be 9 words long less than 32 letters
 password must contain 2 punctuation, 2 letter or more, 2 capital letters, must have 2 more math symbols,  */
 
+/*If the number has many duplicates count the totak number of duplicates entries in an array. 444- 2 duplicaties since 0,1,2.*/
+
+//Api is application programming interface, 
+// The word applications can be thought as a software with an unique function, 
+// the word interface can be thought as a contract of a service between two application. 
+// And then API is basically how 
