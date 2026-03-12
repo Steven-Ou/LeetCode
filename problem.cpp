ee892d9d726fbf0b012b9ee5e15cebff410442f9
Steven-Ou <osteve425@gmail.com>
2026-03-11 23:37:31 -0400

    Adding in

--- problem.cpp
+++ problem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std; 
 /*Create a password that doesnt accept the word hey,bye,password, curse words anyone of these words!
 password has to be 9 words long less than 32 letters
@@ -74,6 +75,10 @@ int fizzbuzz(int x){
 
 int main(){
     int x;
+    string y; 
+    cout<<"Give a word"<<endl;
+    cin>>y;
+    reversal(y);
     cout<<"enter a number:"<<endl;
     cin >>x;
     fizzbuzz(x);
