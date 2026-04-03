b14d0a208d127df8b48631507e723913e5791b19
Steven Ou <osteve425@gmail.com>
2026-04-03 13:59:29 -0400

    learning the right way to use bool

--- Program.cs
+++ Program.cs
@@ -6,23 +6,37 @@ public class Program
     public static void Main()
     {
         List<int> readings = new List<int> { 120, 150, 110, 145, 130, 160 };
-        
+
         int criticalCount = GetCriticalCount(readings);
-        
+
         Console.WriteLine("Critical Readings: " + criticalCount);
     }
 
     public static int GetCriticalCount(List<int> readings)
     {
-        int count=0;
+        int count = 0;
 
-        foreach(int reading in readings)
+        foreach (int reading in readings)
         {
-            if(reading > 140)
+            if (reading > 140)
             {
-                count++; 
+                count++;
             }
         }
-        return count; 
+        return count;
+    }
+
+    public static bool IsSafeToPrescribe(List<string> currentMeds, string newMed)
+    {
+        if (newMed == "Drug A" && currentMeds.Contains("Drug B"))
+        {
+            return false;
+        }
+        if (currentMeds.Contains(newMed))
+        {
+            return false;
+        }
+
+        return true; 
     }
 }
