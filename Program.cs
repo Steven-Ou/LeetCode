06d79774b2752714dcd0951b14a5c5b6479ee271
Steven Ou <osteve425@gmail.com>
2026-04-03 13:27:48 -0400

    Trying out a program to test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> readings = new List<int> { 120, 150, 110, 145, 130, 160 };
+        
+        int criticalCount = GetCriticalCount(readings);
+        
+        Console.WriteLine("Critical Readings: " + criticalCount);
+    }
+
+    public static int GetCriticalCount(List<int> readings)
+    {
+        int count=0;
+
+        foreach(int reading in readings)
+        {
+            if(reading > 140)
+            {
+                count++; 
+            }
+        }
+        return count; 
+    }
+}
