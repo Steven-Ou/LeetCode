1630a0caa83784a31a118c15867c175a2d504844
Steven Ou <osteve425@gmail.com>
2026-04-04 01:08:25 -0400

    Adding in a simplifying way

--- Practice.session.sql
+++ Practice.session.sql
@@ -2,41 +2,52 @@ DROP TABLE IF EXISTS Doctors;
 DROP TABLE IF EXISTS Appointments;
 DROP TABLE IF EXISTS Patients;
 CREATE TABLE Patients (PatientID INTEGER PRIMARY KEY, Name TEXT);
-CREATE TABLE  Appointments (
+CREATE TABLE Doctors (
+    DocID INTEGER PRIMARY KEY,
+    Name TEXT
+);
+CREATE TABLE Appointments (
     ApptID INTEGER PRIMARY KEY,
     PatientID INTEGER,
-    ApptDate DATE
+    DocID INTEGER,
+    ApptDate DATE,
+    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
+    FOREIGN KEY (DocID) REFERENCES Doctors(DocID)
 );
 INSERT INTO Patients (Name)
 VALUES ('Steven'),
     ('Billy'),
     ('Sarah'),
     ('John');
-INSERT INTO Appointments (PatientID, ApptDate)
-VALUES (1, '2026-04-01'),
-    (3, '2026-03-15');
-SELECT p.Name
-FROM Patients p
-    LEFT JOIN Appointments a ON p.PatientID = a.PatientID
-WHERE a.ApptID IS NULL;
-SELECT p.Name,
-    COUNT(a.ApptID) AS TotalAppointments
-FROM Patients p
-    LEFT JOIN Appointments a ON p.PatientID = a.PatientID
-GROUP BY p.Name;
-CREATE TABLE Doctors (
-    DocID INTEGER PRIMARY KEY,
-    Name TEXT
-);
 INSERT INTO Doctors (Name)
 VALUES ('Cindy'),
     ('Leo'),
     ('Charlene'),
     ('Tiffany');
-ALTER TABLE Appointments ADD COLUMN DocID INTEGER;
-UPDATE Appointments SET DocID = 1 WHERE ApptID = 1;
-UPDATE Appointments SET DocID = 2 WHERE ApptID = 2;
+INSERT INTO Appointments (PatientID, DocID, ApptDate)
+VALUES (1, 1, '2026-04-10');
+INSERT INTO Appointments (PatientID, DocID, ApptDate)
+VALUES (3, 2, '2026-04-12');
+SELECT p.Name
+FROM Patients p
+    LEFT JOIN Appointments a ON p.PatientID = a.PatientID
+WHERE a.ApptID IS NULL;
+/* Checks to see who has no appt*/
+SELECT p.Name,
+    COUNT(a.ApptID) AS TotalAppointments
+FROM Patients p
+    LEFT JOIN Appointments a ON p.PatientID = a.PatientID
+GROUP BY p.Name;
 SELECT d.name AS "Doctor With No Appts"
 FROM Doctors d
-    LEFT JOIN Appointment a on d.DocID = a.DocID
+    LEFT JOIN Appointments a on d.DocID = a.DocID
 WHERE a.ApptID IS NULL;
+
+/*Simplifying all top*/
+SELECT 
+    p.Name AS "Patient Name", 
+    d.Name AS "Doctor Name", 
+    a.ApptDate AS "Appointment Date"
+FROM Appointments a
+JOIN Patients p ON a.PatientID = p.PatientID
+JOIN Doctors d ON a.DocID = d.DocID;
