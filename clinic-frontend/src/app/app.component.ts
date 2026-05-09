f0007029cc79591834b85c5f1f162592805ea582
Steven Ou <osteve425@gmail.com>
2026-05-09 01:52:19 -0400

    adding in

--- clinic-frontend/src/app/app.component.ts
+++ clinic-frontend/src/app/app.component.ts
@@ -5,13 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'clinic-frontend';
   doctors = [
     { name: 'Cindy', appointmentCount: 1 },
     { name: 'Leo', appointmentCount: 2 },
-    { name: 'Charlene', appointmentCount: 0 }
+    { name: 'Charlene', appointmentCount: 0 },
   ];
 }
