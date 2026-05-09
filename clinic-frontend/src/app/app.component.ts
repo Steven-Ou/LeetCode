b4c80188cbaff1b9589c86c8f4c8ff28dd7648d6
Steven Ou <osteve425@gmail.com>
2026-05-09 01:55:53 -0400

    adding in

--- clinic-frontend/src/app/app.component.ts
+++ clinic-frontend/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // Ensure CommonModule is here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clinic-frontend';
   doctors = [
-    { name: 'Cindy', appointmentCount: 1 },
-    { name: 'Leo', appointmentCount: 2 },
-    { name: 'Charlene', appointmentCount: 0 },
+    { name: 'Cindy', appointmentCount: 3 },
+    { name: 'Leo', appointmentCount: 1 },
+    { name: 'Charlene', appointmentCount: 0 }
   ];
 }
